db496967721c297ae08b778a47dfad8346a0ab6f
Emanuele Tonello <emanueletonello@gmail.com>
2017-12-14 13:40:18 +0100

    updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ gulp.task('rollup:umd', function () {
       // The name to use for the module for UMD/IIFE bundles
       // (required for bundles with exports)
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
-      moduleName: 'ngx-s3-uploader',
+      name: 'ngx-s3-uploader',
 
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
       globals: {
